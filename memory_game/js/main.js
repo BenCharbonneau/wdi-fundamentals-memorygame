97c758e8bd812165a4748df3683edec0d8617463
Ben Charbonneau <charbobs@Bens-MacBook-Air.local>
2018-03-05 13:15:33 -0600

    finished game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,23 +24,64 @@ var cardsInPlay = [];
 
 function checkForMatch(){
 	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
+		var card1=cardsInPlay[0];
+		var card2=cardsInPlay[1];
+		if (cards[card1].rank === cards[card2].rank) {
 			alert("You found a match!");
+			var score=document.getElementById("score").innerHTML;
+			document.getElementById("score").innerHTML = parseInt(score) + 1;
+			document.querySelector('img[data-id="'+card1+'"]').className = ".matched";
+			document.querySelector('img[data-id="'+card2+'"]').className = ".matched";
 		}
 		else {
 			alert("Sorry, try again.");
+			flipUnmatched();
 		}
+		cardsInPlay = [];
 	}
 }
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute("data-id")
+	if (this.getAttribute("src") !== "images/back.png") {
+		console.log("The card is already flipped.")
+		return;
+	}
 	console.log("User flipped "+cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
+	cardsInPlay.push(cardId);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute("src",cards[cardId].cardImage);
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0;i<cards.length;i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src","images/back.png");
+		cardElement.setAttribute("data-id",i);
+		cardElement.addEventListener('click',flipCard);
+		//cardElement.addEventListener('load',checkForMatch);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
 
+function newGame() {
+	for (var i = 0;i<cards.length;i++) {
+		var cardarr = document.getElementsByTagName("img")[0];
+		document.getElementById("game-board").removeChild(cardarr);
+	}
+	cards.push(cards.shift());
+	cardsInPlay = [];
+	createBoard();
+}
+
+function flipUnmatched() {
+	for (var i = 0;i<cards.length;i++) {
+		if (document.getElementsByTagName("img")[i].className !== ".matched") {
+			document.getElementsByTagName("img")[i].setAttribute("src","images/back.png");
+		}
+	}
+}
+
+createBoard();
 
